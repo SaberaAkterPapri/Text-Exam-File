ac930f14705bf0e1ab51002cb1661d20364b2eb7
Sabera <saberahawladar@gmail.com>
2021-03-26 21:23:55 +0600

    settings updated

--- textexam/settings.py
+++ textexam/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
-STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATICFILES_DIRS = [str(BASE_DIR / "statifiles")]
 # STATICFILES_DIRS = [
 #     STATIC_DIR,
 # ]

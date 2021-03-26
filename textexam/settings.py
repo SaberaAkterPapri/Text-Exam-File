a0a8e58ed727fb604c383b5db81d3398a591a060
Sabera <saberahawladar@gmail.com>
2021-03-26 21:55:04 +0600

    static files change

--- textexam/settings.py
+++ textexam/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
